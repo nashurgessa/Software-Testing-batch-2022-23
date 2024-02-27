940b00ae65582465f736935e71a6f7fc69f3f0b3
nashurgessa <naurgessa@gmail.com>
2024-02-27 16:08:57 +0800

    class2_first_commit

---
+++ class_2/oop_1/src/main/java/org/example/Rectangle.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Rectangle {
+    double height;
+    double width;
+
+    // Constructor
+    Rectangle(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+}
