70e87e8d276bef4fe0ede7108c169469b34a86ed
nashurgessa <naurgessa@gmail.com>
2024-02-27 08:58:23 +0800

    oop_first_commit

---
+++ class_1/oop_1/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+
+    public static void main(String[] args) {
+        int result = add(2, 5);
+        System.out.println("The sum is : "+ result);
+    }
+}
