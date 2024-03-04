1aea7998d6b698ec6b3922723e452f74656e9228
nashurgessa <naurgessa@gmail.com>
2024-03-04 09:26:53 +0800

    class_1first_test

---
+++ class_1/first_test_code/src/main/java/org/example/Calculator.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Calculator {
+
+    public double add(int a, int b) {
+        return a + b;
+    }
+}
--- class_2/oop_1/src/main/java/org/example/Employee.java
+++ class_2/oop_1/src/main/java/org/example/Employee.java
@@ -15,7 +15,7 @@ public class Employee extends Person {
     public String getName() {
         return name;
     }
-    
+
 
     public void setName(String name) {
         this.name = name;
