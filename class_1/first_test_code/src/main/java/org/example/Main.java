63043bbe63176333bda4bbfa743710b189587026
nashurgessa <naurgessa@gmail.com>
2024-03-04 09:36:46 +0800

    test1_commit2

--- class_1/first_test_code/src/main/java/org/example/Main.java
+++ class_1/first_test_code/src/main/java/org/example/Main.java
@@ -4,6 +4,13 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        //
+        Calculator calc = new Calculator();
+        //
+        int a = 3;
+        int b = 4;
+        double result = calc.add(a, b);
+        System.out.println("The result of "+ a + " + "
+                + b + " = " + result);
     }
 }
