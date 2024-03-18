d392ace49dfa622680ec84da9e917f62075960ad
nashurgessa <naurgessa@gmail.com>
2024-03-18 15:28:54 +0800

    test_quadratic_equation

--- class_1/quadratic_equation/src/main/java/com/example/QuadraticEquation.java
+++ class_1/quadratic_equation/src/main/java/com/example/QuadraticEquation.java
@@ -1,5 +1,39 @@
 package com.example;
 
 public class QuadraticEquation {
-	
+	private double a;
+    private double b;
+    private double c;
+    private boolean isValid;
+
+
+    public QuadraticEquation(double a, double b, double c){
+        this.isValid = a < 0 || a > 100 || b< 0 || b> 100 || c< 0 || c > 100;
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+
+    }
+
+    public double discriminant() {
+        return b * b  -  4 * a * c;
+    }
+
+    public String getRoot() {
+        if (isValid) {
+            return "Invalid Input";
+        }
+
+        if (a == 0) {
+            return "Not a quadratic equation";
+        }
+
+        double d = discriminant();
+
+        if (d < 0) return "Imaginary root";
+        else if (d ==0) return "Equal Root";
+        else return "Two Roots";
+    }
 }
--- class_1/quadratic_equation/src/test/java/com/example/QuadraticEquationTest.java
+++ class_1/quadratic_equation/src/test/java/com/example/QuadraticEquationTest.java
@@ -1,13 +1,19 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 public class QuadraticEquationTest {
 	
 	@Test
-	public void NotQuatricEquation(){
+	public void NotQuadraticEquation(){
+		// Arrange
+		QuadraticEquation quEquation = new QuadraticEquation(0, 50, 50);
 
+		//Act
+		String result = quEquation.getRoot();
+
+		// Assert
+		assertEquals("Not a quadratic equation", result, "The result must be not an equation");
 	}
 }
