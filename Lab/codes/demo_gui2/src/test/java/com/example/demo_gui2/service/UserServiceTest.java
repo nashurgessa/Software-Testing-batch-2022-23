e9493a62ffaa2483710911881b8e61551b50a12a
nashurgessa <naurgessa@gmail.com>
2024-04-23 17:26:03 +0800

    update 5

--- Lab/codes/demo_gui2/src/test/java/com/example/demo_gui2/service/UserServiceTest.java
+++ Lab/codes/demo_gui2/src/test/java/com/example/demo_gui2/service/UserServiceTest.java
@@ -1,15 +1,28 @@
 package com.example.demo_gui2.service;
 
 import com.example.demo_gui2.Service.UserService;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserServiceTest {
 
-    UserService userService = new UserService();
+    static UserService userService;
 
+    @BeforeAll
+    static void setUp() {
+        userService = new UserService();
+    }
+    @BeforeEach
+    void beforeEachMethod() {
+        System.out.println("This is before Each");
+    }
+    @AfterEach
+    void afterEachMethod() {
+        System.out.println("This is After Each");
+    }
+    // @Disabled
     @Test
     void testLogin(){
         assertAll("Checking Login",
@@ -19,5 +32,9 @@ public class UserServiceTest {
         );
     }
 
-
+    @Test
+    void registerUserTest() {
+        // Name, email, password
+        assertTrue(userService.registerUser("Alex", "abcd@example.com", "Password123@"));
+    }
 }
