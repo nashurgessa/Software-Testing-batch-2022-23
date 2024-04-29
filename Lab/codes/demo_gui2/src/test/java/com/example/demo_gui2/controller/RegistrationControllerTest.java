b2b5d4b3f7ddd317c42fdeaee927e0926e073d48
nashurgessa <naurgessa@gmail.com>
2024-04-29 17:29:30 +0800

    updates

--- Lab/codes/demo_gui2/src/test/java/com/example/demo_gui2/controller/RegistrationControllerTest.java
+++ Lab/codes/demo_gui2/src/test/java/com/example/demo_gui2/controller/RegistrationControllerTest.java
@@ -4,15 +4,20 @@ import com.example.demo_gui2.common.RegistrationResult;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RegistrationControllerTest {
     static RegistrationController regController;
+    @Disabled
     @BeforeAll
     static void setUp(){
         regController = new RegistrationController();
     }
+    @Disabled
     @Test
     void test1() {
         RegistrationResult result = regController.attemptRegistration(
@@ -23,6 +28,7 @@ public class RegistrationControllerTest {
          */
         assertEquals(RegistrationResult.SUCCESS, result);
     }
+    @Disabled
     @Test
     void test2() {
         RegistrationResult result = regController.attemptRegistration(
@@ -33,22 +39,41 @@ public class RegistrationControllerTest {
          */
         assertEquals(RegistrationResult.PASSWORD_MISMATCH, result);
     }
-
     @Disabled
     @Test
     void test3() {
-
+        RegistrationResult result = regController.attemptRegistration(
+                "Allen", "examplegmail.com",
+                "Password123@", "Password123@");
+        /*
+        result = SUCCESS, REGISTRATION_FAILED, PASSWORD_MISMATCH
+         */
+        assertEquals(RegistrationResult.REGISTRATION_FAILED, result);
     }
-
     @Disabled
     @Test
     void test4() {
 
     }
-
     @Disabled
     @Test
     void test5() {
 
     }
+
+
+    //@ParameterizedTest
+    //@ValueSource(strings = {"Neusoft", "JUnit"})
+    //void withValueSource(String argument) {
+    //    System.out.println("The argument is : "+ argument);
+    //    assertNotNull(argument);
+    //}
+
+    @ParameterizedTest
+    @CsvSource({"1, true", "2, true", "3, false"})
+    void withoutCSVFile (int a, boolean b) {
+        assertEquals(b, a < 3);
+    }
+
+
 }
