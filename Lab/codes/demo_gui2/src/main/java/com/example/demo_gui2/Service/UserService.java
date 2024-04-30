a0870227fc929925ae82c77159660c4a1b5a50e0
nashurgessa <naurgessa@gmail.com>
2024-04-30 17:30:27 +0800

    updates

--- Lab/codes/demo_gui2/src/main/java/com/example/demo_gui2/Service/UserService.java
+++ Lab/codes/demo_gui2/src/main/java/com/example/demo_gui2/Service/UserService.java
@@ -1,18 +1,37 @@
 package com.example.demo_gui2.Service;
 
 import com.example.demo_gui2.common.EmailPasswordValidator;
+import com.example.demo_gui2.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserService {
     EmailPasswordValidator validator = new EmailPasswordValidator();
     // Constructor
     public UserService() {
     }
+
+    // in-memory
+    private final Map<String, User> users = new HashMap<>();
+
     public boolean login(String email, String password) {
         return "user@example.com".equals(email) && "password".equals(password);
     }
     public boolean registerUser(String name, String email, String password) {
+        // Checks if the email and password are valid
         boolean validationResult = validator(email, password);
-        return validationResult;
+        // if email and password are valid, then we create a new user model
+        if(validationResult){
+
+            User newUser = new User(name, email, password);
+            System.out.println("The new user info: "+ newUser.toString());
+            // it is time to save the user
+            users.put("register_user", newUser);
+
+            return true;
+        }
+        return false;
     }
     public boolean validator(String email, String password) {
         return validator.isEmailValid(email) &&
---
+++ Lab/codes/demo_gui2/src/main/java/com/example/demo_gui2/model/User.java
@@ -0,0 +1,50 @@
+package com.example.demo_gui2.model;
+
+public class User {
+    private String name;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+    public User(String name) {
+        this.name = name;
+    }
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "name: "+ name + " email : " + email + " password: "+ password;
+    }
+}
